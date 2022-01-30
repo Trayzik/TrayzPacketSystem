288b0b436ef9d3477dd027c3ec8fc99e981f71f8
Trayz <wycisliktrayz@gmail.com>
2022-01-30 12:22:07 +0100

    Server fix

--- TrayzPacketSystem-server/src/main/java/pl/trayz/packetsystem/PacketSystem.java
+++ TrayzPacketSystem-server/src/main/java/pl/trayz/packetsystem/PacketSystem.java
@@ -25,22 +25,17 @@ public class PacketSystem {
         String hostname;
         try {
             port = args.length != 2 ? 33333 : Integer.parseInt(args[1]);
-            hostname = args.length != 2 ? "127.0.0.1" : args[1];
+            hostname = args.length != 2 ? "127.0.0.1" : args[0];
         }catch (NumberFormatException e) {
             Logger.logError("Enter correct port!");
             System.exit(1);
             return;
         }
-        if(args.length != 2)
-            Logger.logWarning("If you want to use different port than the default, fill startup arguments (ip port)!");
-        startSystem(hostname,port);
-    }
-
-    public static void startSystem(String host, int port) {
+        if(args.length != 2) Logger.logWarning("If you want to use different port than the default, fill startup arguments (ip port)!");
         SERVER_THREAD.submit(() -> {
             ServerSocket server = null;
             try {
-                server = new ServerSocket(port, 50, InetAddress.getByName(host));
+                server = new ServerSocket(port, 50, InetAddress.getByName(hostname));
                 Logger.logSuccess("Successfully started packets system on the port " + port + "!");
 
                 while (true) {
